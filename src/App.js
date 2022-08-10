450e02b3e4ef379ddc1a3aacbdcfd1d11446c892
Apurv Vyas <76184941+apurv1997@users.noreply.github.com>
2022-08-11 00:07:56 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import React, {Component} from 'react';
+import Navbar from './components/Navbar';
+import News from './components/News';
+
+export default class App extends Component {
+  
+  render(){
+    return (
+      <div>
+    <Navbar/>
+    <News/>
+      </div>
+    )
+  }
+}
+
+//API Key: e9475999c2494ad0b698214a92065581
+// url: https://newsapi.org/v2/top-headlines?country=in&apiKey=e9475999c2494ad0b698214a92065581
