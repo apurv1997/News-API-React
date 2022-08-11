1d330efb5e454159198da3d3aa5f7120c571c34b
Apurv Vyas <76184941+apurv1997@users.noreply.github.com>
2022-08-11 13:13:21 +0530

    Update NewsItem.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
    let {title, description, imageUrl, newsUrl} = this.props
     return (
       <div className="my-3">
-         <div className="card" style={{width: "18rem"}}>
+         <div className="card">
             <img src={!imageUrl?"https://i0.wp.com/9to5mac.com/wp-content/uploads/sites/6/2019/09/Screen-Shot-2019-09-10-at-13.03.17-PM.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
